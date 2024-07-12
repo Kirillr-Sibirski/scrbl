e889368d1b48a9219bee31f8b88044126a19312a
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2024-07-13 01:55:44 +0200

    Liquidate reverts

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -8,7 +8,8 @@ import { IWorldID } from './interfaces/IWorldID.sol';
 contract Manager {
 	using ByteHasher for bytes;
 
-	int8 internal constant DEFAULT_CREDIT_SCORE = 50;
+	int8 private constant DEFAULT_CREDIT_SCORE = 50;
+	uint256 private constant INTEREST_INTERVAL = 86400; //seconds (1 day)
 
 	struct Loan {
 		uint256 debtAmount; // In USDC, will be increasing with interested rate
@@ -36,6 +37,8 @@ contract Manager {
 	/// @dev The World ID group ID (always 1)
 	uint256 internal immutable groupId = 1;
 
+	uint256 private lastInterestTopUp = block.timestamp;
+
 	/// @dev Normal wallet to WorldID (here are the wallets that have been verified with the World ID)
 	mapping(address => uint256) internal s_verifiedWallet;
 
@@ -44,6 +47,7 @@ contract Manager {
 
 	/// @dev List of current loans
 	mapping(address => Loan) internal s_loans;
+	address[] internal s_loanAddresses;
 
 	/// @param nullifierHash The nullifier hash for the verified proof
 	/// @dev A placeholder event that is emitted when a user successfully verifies with World ID
@@ -55,6 +59,8 @@ contract Manager {
 	event UpdateVerified(uint256 nullifierHash, address old_address, address new_address);
 
 	event RepayLoan(address, uint256);
+
+	event Liquidation(address);
 
 	/// @param _worldId The WorldID router that will verify the proofs
 	/// @param _appId The World ID app ID
@@ -116,16 +122,16 @@ contract Manager {
 
 		if(creditScore >= 90) { // The best terms for a loan
 			collateralAmount = 0.02 ether; /// Get price of ETH to USDC, get 10% of the @param loanAmount 
-			interestRate = 10; // %
+			interestRate = 0.0274; // % per day (10% per year)
 		} else if(creditScore < 90 && creditScore >= 60) {
 			collateralAmount = 0.03 ether; /// Get price of ETH to USDC, get 30% of the @param loanAmount 
-			interestRate = 20; // %
+			interestRate = 0.0548; // % per day (20% per year)
 		} else if(creditScore < 60 && creditScore >= 30) {
 			collateralAmount = 0.04 ether; /// Get price of ETH to USDC, get 60% of the @param loanAmount 
-			interestRate = 30; // %
+			interestRate = 0.0822; // % per day (30% per year)
 		} else { // The worst terms for a loan
 			collateralAmount = 0.05 ether; /// Get price of ETH to USDC, get 80% of the @param loanAmount 
-			interestRate = 50; // %
+			interestRate = 0.137; // % per day (50% per year)
 		}
 	}
 
@@ -138,13 +144,12 @@ contract Manager {
 		// Deploy new wallet and fund with loanAmount in USDC
 		address escrowWallet = address(0); // Actual address here
 		s_loans[msg.sender] = (loanAmount, collateralAmount, escrowWallet, interestRate);
+		s_loanAddresses.push(msg.sender);
 	}
-
 
 	/// @dev This function is used to improve the health ratio of user's loan
 	function repayWithoutCollateralWithdrawal(uint256 repayAmount) external {
 		if(repayAmount = 0) revert ("Amount must be greater than zero");
-
         // Transfer USDC from the user to the contract
         bool success = usdcToken.transferFrom(msg.sender, address(this), repayAmount);
         require(success, "USDC transfer failed");
@@ -155,15 +160,31 @@ contract Manager {
         emit RepayLoan(msg.sender, repayAmount);
 	}
 
-	function checkLiquidate(address debtor) external {
-		// check that loan exists
+	/// @dev This function is used to fully repay and terminate the loan
+	function repayFull() external {
+		// Delete s_loans shit
 	}
 
-	function liquidateLoan() external {
+	function checkLiquidate(address debtor) external returns(bool liquidate) {
+		if(s_loans[msg.sender].debtAmount == 0) revert("This loan doesn't exist.");
+		if(true /* health ratio is bad */) {
+			liquidate = false;
+		} else {
+			liquidate = true;
+		}
+	}
+
+	function liquidateLoan(address debtor) external {
 		// 1Inch liquidation event here
+		// Decrease credit score
+		emit Liquidation(debtor);
 	}
 
 	function topUpInterestRate() external {
 		// Do a check that enough time has passed
+		if(block.timestamp < (lastInterestTopUp+INTEREST_INTERVAL)) revert("Not enough time has passed");
+        for (uint256 i = 0; i < s_loanAddresses.length; i++) {
+			// Loop through each loan and increase debtAmount by whatever their interest rate is.
+        }
 	}
 }

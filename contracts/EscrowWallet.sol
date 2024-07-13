ecdc0c5ba5fde71b3aa8d1e7a7abf727da1202bf
Cjpotter10 <caleb10joseph@gmail.com>
2024-07-13 13:35:38 +0200

    pushing up to change branch

--- contracts/EscrowWallet.sol
+++ contracts/EscrowWallet.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.13;
 pragma abicoder v1;
 
 import {IEscrowWallet} from "./interfaces/IEscrowWallet.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "solidity-utils/libraries/SafeERC20.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
 
 // @notice thrown when someone other than the escrow manager is trying to call this function
 error CallerNotEscrowManagerException();

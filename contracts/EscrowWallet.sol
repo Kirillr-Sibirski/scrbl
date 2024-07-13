752dc1b09f81ac48ca703e2362762c07991a95ff
Cjpotter10 <caleb10joseph@gmail.com>
2024-07-13 08:32:42 +0200

    pushing up files to install 1inch solidity-utils package

---
+++ contracts/EscrowWallet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+pragma abicoder v1;
+
+import {IEscrowWallet} from "./interfaces/IEscrowWallet.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@1inch/solidity-utils/contracts/libraries/SafeERC20.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+
+

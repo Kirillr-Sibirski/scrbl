e915d00c3a3715cbf4345c6ef9d0e29a3a5e8700
Tymur-Tykva <tykva.tymur@gmail.com>
2024-07-13 11:49:49 +0200

    Continued web3 integration

--- components/header.tsx
+++ components/header.tsx
@@ -23,14 +23,11 @@ export default function Header() {
 			<div className="container h-full flex flex-row items-center gap-6">
 				<h1 className="text-2xl font-bold [text-shadow:_1px_2px_0_rgb(0_0_0_/_40%)]">SCRBL</h1>
 
-				<div className="min-w-[2px] h-[70%] bg-l-100/[0.2] mx-6"></div>
+				<div className="min-w-[2px] h-[70%] bg-l-100/[0.2] mx-6"/>
 
 				{links &&
 					links.map(({ href, label, style }, i) => (
-						<Link
-							key="i"
-							href={href}
-							className={twMerge(
+						<Link key="i" href={href} className={twMerge(
 								"px-4 py-2 text-center text-l-200 font-semibold rounded-lg border-[1px] hover:brightness-125 active:scale-95 transition duration-200",
 								pathname == href ? style.active : style.main
 							)}
@@ -38,6 +35,8 @@ export default function Header() {
 							{label}
 						</Link>
 					))}
+
+				<div className="min-w-[2px] h-[70%] bg-l-100/[0.2] ml-auto mr-6"/>
 			</div>
 		</header>
 	)

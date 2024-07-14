141e510737246cf478d39598b53f429ec0eefef3
Cjpotter10 <caleb10joseph@gmail.com>
2024-07-14 05:37:34 +0200

    commiting changes to install package in right place

--- contracts/src/EscrowFacade.sol
+++ contracts/src/EscrowFacade.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.12;
 pragma abicoder v2;
 
-import { ISwapRouter } from "v3-periphery/interfaces/ISwapRouter.sol";
+import { ISwapRouter02 } from "swap-router-contracts/interfaces/ISwapRouter02.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "solidity-utils/libraries/SafeERC20.sol";
 import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
@@ -27,7 +27,7 @@ contract EscrowFacade {
 
     Manager public immutable escrowManager;
     address public immutable swapRouterAddr = 0x94cC0AaC535CCDB3C01d6787D6413C739ae12bc4; //Base sepolia SwapRouter02 contract address 
-    ISwapRouter public immutable swapRouter = ISwapRouter(swapRouterAddr);
+    ISwapRouter02 public immutable swapRouter = ISwapRouter02(swapRouterAddr);
 
     address public constant WETH = 0xf531B8F309Be94191af87605CfBf600D71C2cFe0; //base sepolia address
     address public constant USDC = 0x5dEaC602762362FE5f135FA5904351916053cF70; //base sepolia address
@@ -55,7 +55,7 @@ contract EscrowFacade {
         bool approved = abi.decode(result, (bool));
         if (approved == false) revert("unable to approve transfer for token0 in escrow wallet");
 
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+        ISwapRouter02.ExactInputSingleParams memory params = ISwapRouter02
             .ExactInputSingleParams({
                 tokenIn: tokenIn,
                 tokenOut: tokenOut,
